94cdcc6a2c1637790124fa7d25d1a03433dbd660
Yasha Mostofi <yasha.mostofi@gmail.com>
2014-05-24 22:10:12 -0500

    Revert 5a19243..738c80b
    This rolls back to commit 5a19243ff8035cb503a3059c1c5536bb43c03155.

--- notefeed/notefeed.js
+++ notefeed/notefeed.js
@@ -11,12 +11,6 @@ Meteor.methods({
     // if (you want to throw an error)
     //   throw new Meteor.Error(404, "Can't find my pants");
     // return "some return value";
-  },
-
-  addSubscription: function (name, classID) {
-    var user = Meteor.users.findOne({'username': name});
-    var subscriptionList = user.sub_classes;
-
   }
 });
 
@@ -29,37 +23,6 @@ if (Meteor.isClient) {
   //   sendVerificationEmail: 'true'
   // });
 
-  Meteor.autosubscribe(function() {
-    classes.find().observe({
-      added: function(item) {
-        var newHTML = '';
-
-        for (var classy in classes.find())
-        {
-          newHTML = newHTML + '<div class="btn-group" id="browseClass_'+classy._id;
-          newHTML = newHTML +'"><button class="btn btn-default" type="button" data-toggle="modal" data-target="#'+classy._id+'">';
-          newHTML = newHTML +classy.name+' by '+classy.user+' (rating: '+classy.rating+')</button>';
-          newHTML = newHTML + '<button class="btn btn-default dropdown-toggle" data-toggle="dropdown" type="button"><span class="caret">';
-          newHTML = newHTML + '</span><span class="sr-only"></span>';
-          newHTML = newHTML + '</button><ul class="dropdown-menu" role="menu"><li><a href="#" data-toggle="modal" data-target="#';
-          newHTML = newHTML +classy._id+'">View Class</a></li>';
-          newHTML = newHTML + '<li><a href="#" id="subscribeButton">Subscribe</a></li></ul><br/></div>';
-          newHTML = newHTML + '<div class="modal fade" id="'+classy._id+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">';
-          newHTML = newHTML + '<div class="modal-dialog"><div class="modal-content"><div class="modal-header">';
-          newHTML = newHTML + '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button></div>';
-          newHTML = newHTML + '<div class="modal-body">this is class '+classy.name+'</div>';
-          newHTML = newHTML + '<div class="modal-footer">';
-          newHTML = newHTML + '<button type="button" class="btn btn-success" data-dismiss="modal" id="subscribeButton">Subscribe!</button>';
-          newHTML = newHTML + '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div>';
-        }
-
-        $("#browseClassesTemplate").html(newHTML);
-        $("#browseClassesDiv").html("{{> browseClasses}}");
-
-     }
-    });
-  });
-
   Meteor.subscribe('userData');
   Meteor.subscribe('classes');
   
@@ -68,30 +31,6 @@ if (Meteor.isClient) {
     // $("#browseClassesDiv").hide();
     // $("#userProfile").hide();
     $(".modal").hide();
-
-    var newHTML = '';
-
-    for (var classy in classes.find())
-    {
-      newHTML = newHTML + '<div class="btn-group" id="browseClass_'+classy._id;
-      newHTML = newHTML +'"><button class="btn btn-default" type="button" data-toggle="modal" data-target="#'+classy._id+'">';
-      newHTML = newHTML +classy.name+' by '+classy.user+' (rating: '+classy.rating+')</button>';
-      newHTML = newHTML + '<button class="btn btn-default dropdown-toggle" data-toggle="dropdown" type="button"><span class="caret">';
-      newHTML = newHTML + '</span><span class="sr-only"></span>';
-      newHTML = newHTML + '</button><ul class="dropdown-menu" role="menu"><li><a href="#" data-toggle="modal" data-target="#';
-      newHTML = newHTML +classy._id+'">View Class</a></li>';
-      newHTML = newHTML + '<li><a href="#" id="subscribeButton">Subscribe</a></li></ul><br/></div>';
-      newHTML = newHTML + '<div class="modal fade" id="'+classy._id+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">';
-      newHTML = newHTML + '<div class="modal-dialog"><div class="modal-content"><div class="modal-header">';
-      newHTML = newHTML + '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button></div>';
-      newHTML = newHTML + '<div class="modal-body">this is class '+classy.name+'</div>';
-      newHTML = newHTML + '<div class="modal-footer">';
-      newHTML = newHTML + '<button type="button" class="btn btn-success" data-dismiss="modal" id="subscribeButton">Subscribe!</button>';
-      newHTML = newHTML + '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div>';
-    } 
-
-  $("#browseClassesTemplate").html(newHTML);
-  $("#browseClassesDiv").html("{{> browseClasses}}");
   });
 
   // Template.menubar.events({
@@ -128,12 +67,6 @@ if (Meteor.isClient) {
       Meteor.call('addClass', name, user, keywords, startDate, endDate, freq);
       
       //$("#newClass_modal_body").val("Class creation successful!");
-    }
-  });
-
-  Template.browseClasses.events({
-    'click #subscribeButton' : function () {
-      Meteor.call("addSubscription", Meteor.user().username);
     }
   });
 
