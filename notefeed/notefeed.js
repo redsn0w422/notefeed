99bdbcae8b9d4ce2858a5abaee554cbf06dbbd2f
Yasha Mostofi <yasha.mostofi@gmail.com>
2014-11-08 16:19:36 -0600

    got rid of stupid comments

--- notefeed/notefeed.js
+++ notefeed/notefeed.js
@@ -5,12 +5,6 @@ Meteor.methods({
 
     classes.insert({'name':name, 'user':user, 'keywords':keywords, 'startDate':startDate,
         'endDate':endDate, 'freq':freq, 'notes':[], 'ratingTotal': 0, 'numRatings': 0, 'rating': null});
-    //check(arg1, String);
-    //check(arg2, [Number]);
-    // .. do stuff ..
-    // if (you want to throw an error)
-    //   throw new Meteor.Error(404, "Can't find my pants");
-    // return "some return value";
   },
 
   addSubscription: function (name, classID) {
@@ -54,95 +48,14 @@ if (Meteor.isClient) {
     passwordSignupFields: 'USERNAME_AND_EMAIL'
   });
 
-  // Accounts.config({
-  //   sendVerificationEmail: 'true'
-  // });
-
-  // Meteor.autosubscribe(function() {
-  //   classes.find().observe({
-  //     added: function(item) {
-  //       var newHTML = '';
-
-  //       for (var classy in classes.find())
-  //       {
-  //         newHTML = newHTML + '<div class="btn-group" id="browseClass_'+classy._id;
-  //         newHTML = newHTML +'"><button class="btn btn-default" type="button" data-toggle="modal" data-target="#'+classy._id+'">';
-  //         newHTML = newHTML +classy.name+' by '+classy.user+' (rating: '+classy.rating+')</button>';
-  //         newHTML = newHTML + '<button class="btn btn-default dropdown-toggle" data-toggle="dropdown" type="button"><span class="caret">';
-  //         newHTML = newHTML + '</span><span class="sr-only"></span>';
-  //         newHTML = newHTML + '</button><ul class="dropdown-menu" role="menu"><li><a href="#" data-toggle="modal" data-target="#';
-  //         newHTML = newHTML +classy._id+'">View Class</a></li>';
-  //         newHTML = newHTML + '<li><a href="#" id="subscribeButton">Subscribe</a></li></ul><br/></div>';
-  //         newHTML = newHTML + '<div class="modal fade" id="'+classy._id+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">';
-  //         newHTML = newHTML + '<div class="modal-dialog"><div class="modal-content"><div class="modal-header">';
-  //         newHTML = newHTML + '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button></div>';
-  //         newHTML = newHTML + '<div class="modal-body">this is class '+classy.name+'</div>';
-  //         newHTML = newHTML + '<div class="modal-footer">';
-  //         newHTML = newHTML + '<button type="button" class="btn btn-success" data-dismiss="modal" id="subscribeButton">Subscribe!</button>';
-  //         newHTML = newHTML + '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div>';
-  //       }
-
-  //       $("#browseClassesTemplate").html(newHTML);
-  //       $("#browseClassesDiv").html("{{> browseClasses}}");
-
-  //    }
-  //   });
-  // });
-
   Meteor.subscribe('userData');
   Meteor.subscribe('classes');
   
   Meteor.startup(function () {
-    // $("#newClassForm").hide();
-    // $("#browseClassesDiv").hide();
-    // $("#userProfile").hide();
     $(".modal").hide();
     Session.set("filterVal", "1");
   });
-  //   var newHTML = '';
 
-  //   for (var classy in classes.find())
-  //   {
-  //     newHTML = newHTML + '<div class="btn-group" id="browseClass_'+classy._id;
-  //     newHTML = newHTML +'"><button class="btn btn-default" type="button" data-toggle="modal" data-target="#'+classy._id+'">';
-  //     newHTML = newHTML +classy.name+' by '+classy.user+' (rating: '+classy.rating+')</button>';
-  //     newHTML = newHTML + '<button class="btn btn-default dropdown-toggle" data-toggle="dropdown" type="button"><span class="caret">';
-  //     newHTML = newHTML + '</span><span class="sr-only"></span>';
-  //     newHTML = newHTML + '</button><ul class="dropdown-menu" role="menu"><li><a href="#" data-toggle="modal" data-target="#';
-  //     newHTML = newHTML +classy._id+'">View Class</a></li>';
-  //     newHTML = newHTML + '<li><a href="#" id="subscribeButton">Subscribe</a></li></ul><br/></div>';
-  //     newHTML = newHTML + '<div class="modal fade" id="'+classy._id+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">';
-  //     newHTML = newHTML + '<div class="modal-dialog"><div class="modal-content"><div class="modal-header">';
-  //     newHTML = newHTML + '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button></div>';
-  //     newHTML = newHTML + '<div class="modal-body">this is class '+classy.name+'</div>';
-  //     newHTML = newHTML + '<div class="modal-footer">';
-  //     newHTML = newHTML + '<button type="button" class="btn btn-success" data-dismiss="modal" id="subscribeButton">Subscribe!</button>';
-  //     newHTML = newHTML + '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div>';
-  //   } 
-
-  // $("#browseClassesTemplate").html(newHTML);
-  // $("#browseClassesDiv").html("{{> browseClasses}}");
-  // });
-
-  // Template.menubar.events({
-  //   'click #newClass' : function () {
-  //     $("#newClassForm").show();
-  //     $("#browseClassesDiv").hide();
-  //     $("#userProfile").hide();
-
-  //   },
-  //   'click #browseClasses' : function () {
-  //     $("#newClassForm").hide();
-  //     $("#browseClassesDiv").show();
-  //     $("#userProfile").hide();
-
-  //   },
-  //   'click #meLink' : function () {
-  //     $("#userProfile").show();
-  //     $("#browseClassesDiv").hide();
-  //     $("#newClassForm").hide();
-  //   }
-  // });
 
   Template.menubar.events({
     'click #searchFilter': function () {
@@ -160,18 +73,8 @@ if (Meteor.isClient) {
       var freq = $("#newClass_freq").val();
 
       Meteor.call('addClass', name, user, keywords, startDate, endDate, freq);
-      
-      //$("#newClass_modal_body").val("Class creation successful!");
     }
   });
-
-  // Template.uploadPdf.events({
-  //     'change input': function(ev) {  
-  //       _.each(ev.currentTarget.files, function(file) {
-  //       Meteor.saveFile(file, file.name);
-  //     });
-  //   }
-  // });
 
   Template.menubar.events({
     'click #search' : function (event) {
@@ -196,11 +99,6 @@ if (Meteor.isClient) {
       Meteor.saveFile(file, file.name);
       Meteor.call("addNotes", file.name, classID);
 
-      // Meteor.call('sendEmail',
-      //          "sonofthebrownguy@gmail.com",
-      //          "yasha.mostofi@gmail.com",
-      //          "update!",
-      //          "new notes!");
       var cursor = Meteor.users.find();
       cursor.forEach(function(user) 
       {
